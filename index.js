86e340b4876bd009b92af7475c2d6bd499c7f623
FacuM <facumo.fm@gmail.com>
2019-02-12 23:20:50 -0300

    Drop unused initialization

--- index.js
+++ index.js
@@ -3,12 +3,7 @@ $(document).ready(function() {
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
     $(".dropdown-button").dropdown();
-    $(".button-collapse").sideNav();
-    $('select').material_select();
-    $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15 // Creates a dropdown of 15 years to control year
-    });
-    $('.modal-trigger').leanModal();
+    //$(".button-collapse").sideNav();
+    $('select').formSelect();
     $('.carousel').carousel();
 });
