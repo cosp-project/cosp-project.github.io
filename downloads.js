eab3ac6285dc6dc1157d029d218563b1491b5227
FacuM <facumo.fm@gmail.com>
2019-02-15 03:21:36 -0300

    Add localStorage caching for all the API requests!

--- downloads.js
+++ downloads.js
@@ -2,13 +2,25 @@
 var atime = 500;
 
 // Miscelaneous global variables initialization
-var startup = true; var mDeviceList = null; var debug = false;
+var startup = true; var mDeviceList = null; var debug = true; var mStorExpirationTime = 1; // (hours)
 
 // Build list server URL
 var mBuildListURL = 'https://cosp-webserver.herokuapp.com';
 
 // Device list server url
 var mDeviceListURL = 'https://mirror.codebucket.de/cosp/getdevices.php';
+
+// Check if HTML5 blob storage is available.
+if (typeof(Storage) !== 'undefined')
+{
+  logi('HML5 blob storage support detected.');
+  var mHasHTML5Stor = true;
+}
+else
+{
+  logw('HML5 blob storage support is missing or broken.');
+  var mHasHTML5Stor = false;
+}
 
 // Display debugging info in console (only if enabled)
 function logi(info) { if (debug) { console.log(info); } }
@@ -78,45 +90,126 @@ function getDate(datestr)
 // Build the table for the selected entry available
 function update()
 {
+  DeviceCodename = $('#devices option:selected').val();
   if ($('#builds').length || $('.err').length)
   {
     $('#builds').remove(); $('.err').remove();
   }
-  // Request the latest download.
-  reqURL = mBuildListURL + '/latestDownload';
-  logi('Ajax: Retrieving "' + reqURL + '"...');
-  $.ajax({ type: 'GET', url: reqURL, data: { device: $('#devices option:selected').text() }, dataType: 'json' })
-  .done(function(basic_json_object) { logi('Ajax: success!');
-    // Request more details about the build.
-    reqURL = mBuildListURL + '/checkUpdate';
+  if (mHasHTML5Stor)
+  {
+    if ((localStorage.getItem(DeviceCodename + '_basic') === null && localStorage.getItem(DeviceCodename + '_advanced') === null && localStorage.getItem(DeviceCodename + '_failure') === null) || (localStorage.getItem('global_expiration') === null || (getTime() > localStorage.getItem('global_expiration'))))
+    {
+      // Request the latest download.
+      reqURL = mBuildListURL + '/latestDownload';
+      logi('Ajax: Retrieving "' + reqURL + '"...');
+      $.ajax({ type: 'GET', url: reqURL, data: { device: DeviceCodename }, dataType: 'json' })
+      .done(function(basic_json_object) {
+        logi('Ajax: success!');
+        logi('Saving the basic build information for ' + DeviceCodename + ' as a HTML5 web storage item "' + DeviceCodename + '_basic"...');
+        localStorage.setItem(DeviceCodename + '_basic', JSON.stringify(basic_json_object));
+        // Request more details about the build.
+        reqURL = mBuildListURL + '/checkUpdate';
+        logi('Ajax: Retrieving "' + reqURL + '"...');
+        $.ajax({ type: 'GET', url: reqURL, data: { device: DeviceCodename, date: basic_json_object.date }, dataType: 'json' })
+        .done(function(advanced_json_object) {
+          logi('Ajax: success!');
+          logi('Saving the advanced build information for ' + DeviceCodename + ' as a HTML5 web storage item "' + DeviceCodename + '_advanced"...');
+          localStorage.setItem(DeviceCodename + '_advanced', JSON.stringify(advanced_json_object));
+          StorExpirationTime = (mStorExpirationTime * 3600000);
+          logi('All the required data has been gathered, an expiration date has been set.')
+          logi('The cached data will expire in ' + mStorExpirationTime + ' hour(s) (or ' + StorExpirationTime + ' ms).');
+          StorExpirationTime += getTime();
+          localStorage.setItem('global_expiration', StorExpirationTime);
+          $('.info').remove();
+          // Reuse date as MMddYY.
+          DeviceBuildData = [ basic_json_object.date, advanced_json_object.changeLog, basic_json_object.download ];
+          rendertable(DeviceBuildData);
+         })
+        .fail(function (response) {
+          if (response.responseText == 'error')
+          {
+            loge('Ajax: the API reported an error.'); $('.info').remove(); $('#devices').append('<p class="info">No builds found.</p>');
+          }
+        });
+      })
+      .fail(function (response) {
+        if (response.responseText == 'error')
+        {
+          localStorage.setItem(DeviceCodename + '_failure', response.responseText);
+          loge('Ajax: the API reported an error.'); $('.info').remove(); $('#devices').append('<p class="info">No builds found.</p>');
+        }
+        else
+        {
+          if (typeof(response.responseText) != 'string')
+          {
+            loge('Ajax: the API reported an error, the "responseText" property of "response" isn\'t a string. Is the server down? No internet connection?'); $('.info').remove(); $('#devices').append('<p class="err">Couldn\'t reach server.</p>');
+          }
+        }
+      });
+    }
+    else
+    {
+      $('.info').remove();
+      logi('HTML5 web storage entries have been found for the selected device "' + DeviceCodename + '".');
+      logi('Found: ');
+      logi(' - ' + localStorage.getItem(DeviceCodename + '_basic') === null ? '' : DeviceCodename + '_basic: ' + localStorage.getItem(DeviceCodename + '_basic'));
+      logi(' - ' + localStorage.getItem(DeviceCodename + '_advanced') === null ? '' :  DeviceCodename + '_advanced: ' + localStorage.getItem(DeviceCodename + '_advanced'));
+      logi(' - ' + localStorage.getItem(DeviceCodename + '_failure') === null ? '' : DeviceCodename + '_failure: ' + localStorage.getItem(DeviceCodename + '_failure'));
+      if (localStorage.getItem(DeviceCodename + '_failure') === 'error')
+      {
+       $('#devices').append('<p class="info">No builds found.</p>');
+      }
+      else
+      {
+       DeviceBuildData = [ JSON.parse(localStorage.getItem(DeviceCodename + '_basic')).date, JSON.parse(localStorage.getItem(DeviceCodename + '_advanced')).changeLog, JSON.parse(localStorage.getItem(DeviceCodename + '_basic')).download ];
+       rendertable(DeviceBuildData);
+      }
+      StorExpirationTimeRem = ((localStorage.getItem('global_expiration') - getTime()) / 1000).toFixed(2);
+      logi(StorExpirationTimeRem + ' seconds remaining before refreshing the cache.');
+    }
+  }
+  else
+  {
+    // Request the latest download.
+    reqURL = mBuildListURL + '/latestDownload';
     logi('Ajax: Retrieving "' + reqURL + '"...');
-    $.ajax({ type: 'GET', url: reqURL, data: { device: $('#devices option:selected').text(), date: basic_json_object.date }, dataType: 'json' })
-    .done(function(advanced_json_object) {
-      logi('Ajax: success!'); $('.info').remove();
-      // Reuse date as MMddYY.
-      DeviceBuildData = [ basic_json_object.date, advanced_json_object.changeLog, basic_json_object.download ];
-      rendertable(DeviceBuildData);
-     })
+    $.ajax({ type: 'GET', url: reqURL, data: { device: DeviceCodename }, dataType: 'json' })
+    .done(function(basic_json_object) {
+      logi('Ajax: success!');
+      logi('Saving the basic build information for ' + DeviceCodename + ' as a HTML5 web storage item "' + DeviceCodename + '_basic"...');
+      // Request more details about the build.
+      reqURL = mBuildListURL + '/checkUpdate';
+      logi('Ajax: Retrieving "' + reqURL + '"...');
+      $.ajax({ type: 'GET', url: reqURL, data: { device: DeviceCodename, date: basic_json_object.date }, dataType: 'json' })
+      .done(function(advanced_json_object) {
+        logi('Ajax: success!');
+        logi('Saving the advanced build information for ' + DeviceCodename + ' as a HTML5 web storage item "' + DeviceCodename + '_advanced"...');
+        $('.info').remove();
+        // Reuse date as MMddYY.
+        DeviceBuildData = [ basic_json_object.date, advanced_json_object.changeLog, basic_json_object.download ];
+        rendertable(DeviceBuildData);
+       })
+      .fail(function (response) {
+        if (response.responseText == 'error')
+        {
+          loge('Ajax: the API reported an error.'); $('.info').remove(); $('#devices').append('<p class="info">No builds found.</p>');
+        }
+      });
+    })
     .fail(function (response) {
       if (response.responseText == 'error')
       {
         loge('Ajax: the API reported an error.'); $('.info').remove(); $('#devices').append('<p class="info">No builds found.</p>');
       }
-    });
-  })
-  .fail(function (response) {
-    if (response.responseText == 'error')
-    {
-      loge('Ajax: the API reported an error.'); $('.info').remove(); $('#devices').append('<p class="info">No builds found.</p>');
-    }
-    else
-    {
-      if (typeof(response.responseText) != 'string')
+      else
       {
-        loge('Ajax: the API reported an error, the "responseText" property of "response" isn\'t a string. Is the server down? No internet connection?'); $('.info').remove(); $('#devices').append('<p class="err">Couldn\'t reach server.</p>');
+        if (typeof(response.responseText) != 'string')
+        {
+          loge('Ajax: the API reported an error, the "responseText" property of "response" isn\'t a string. Is the server down? No internet connection?'); $('.info').remove(); $('#devices').append('<p class="err">Couldn\'t reach server.</p>');
+        }
       }
-    }
-  });
+    });
+  }
 }
 
 function rendertable(DeviceBuildList)
@@ -136,12 +229,16 @@ function rendertable(DeviceBuildList)
   $('#device-list').append(table(DeviceBuildTable));
 }
 
-function showdevices()
+function showdevices(DeviceList)
 {
+  $('.preloader-wrapper').fadeOut(atime);
+  setTimeout(function(){
+    $('.preloader-wrapper').remove();
+  }, atime);
   DevicePicker = '';
-  for (i = 0; i < mDeviceList.length; i++)
+  for (i = 0; i < DeviceList.length; i++)
   {
-   DevicePicker = DevicePicker + option(mDeviceList[i], mDeviceList[i]);
+   DevicePicker = DevicePicker + option(DeviceList[i], DeviceList[i]);
   }
   setTimeout(function()
   {
@@ -154,13 +251,39 @@ function showdevices()
 }
 
 // Start the Ajax request and run the functions, build the available device list.
-reqURL = mDeviceListURL;
-logi('Ajax: Retrieving "' + reqURL + '"...');
-$.ajax({ type: 'GET', url: reqURL, dataType: 'json' })
-.always(function() { $('.preloader-wrapper').fadeOut(atime); setTimeout(function() { $('.preloader-wrapper').remove() }, atime); })
-.done(function (json_array) { logi('Ajax: success!'); mDeviceList = json_array; showdevices(); } )
-.fail(function() { loge('Ajax: the API reported an error.'); $('#devices').append('<p class="err">Unable to retrieve device list.</p>'); }
-);
+if (mHasHTML5Stor)
+{
+  if (localStorage.getItem('mDeviceList') === null)
+  {
+    reqURL = mDeviceListURL;
+    logi('Ajax: Retrieving "' + reqURL + '"...');
+    $.ajax({ type: 'GET', url: reqURL, dataType: 'json' })
+    .done(function (json_array)
+    {
+      logi('Ajax: success!');
+      mDeviceList = json_array;
+      showdevices(mDeviceList);
+      logi('Saving mDeviceList as a HTML5 web storage item "mDeviceList"...');
+      localStorage.setItem('mDeviceList', JSON.stringify(mDeviceList));
+    })
+    .fail(function() { loge('Ajax: the API reported an error.'); $('#devices').append('<p class="err">Unable to retrieve device list.</p>'); }
+    );
+  }
+  else
+  {
+    showdevices(JSON.parse(localStorage.mDeviceList));
+  }
+}
+else
+{
+  reqURL = mDeviceListURL;
+  logi('Ajax: Retrieving "' + reqURL + '"...');
+  $.ajax({ type: 'GET', url: reqURL, dataType: 'json' })
+  .always(function() { $('.preloader-wrapper').fadeOut(atime); setTimeout(function() { $('.preloader-wrapper').remove() }, atime); })
+  .done(function (json_array) { logi('Ajax: success!'); mDeviceList = json_array; showdevices(); } )
+  .fail(function() { loge('Ajax: the API reported an error.'); $('#devices').append('<p class="err">Unable to retrieve device list.</p>'); }
+  );
+}
 
 $(document).ready(function()
 {
@@ -168,5 +291,5 @@ $(document).ready(function()
   $('#devices').on('change', function() {
     update();
   });
-  if (!debug) { console.log('Hello there! It might be easier to contribute if you enable the debugging code. To do so, simply type as follows: \n\ndebug = true;\n\nThen, all the following actions will have an increased console verbosity.') }
+  if (!debug) { console.log('Hello there! It might be easier to contribute if you enable the debugging code. To do so, simply type as follows: \n\ndebug = true;\n\nThen, all the following actions will have an increased console verbosity.\n\nFeel free to clear the cache with the following code:\n\nlocalStorage.clear();') }
 });
